9fa880fdd6f47f1a0752dcb00ccff7396787a271
azabelo <azabelo21old@gmail.com>
2023-07-11 11:14:53 -0700

    transformer

--- transformer_pipeline.py
+++ transformer_pipeline.py
@@ -112,7 +112,7 @@ class Classifier(nn.Module):
         super().__init__()
         self.feature_extractor = model
         self.classifier = nn.Sequential(
-            nn.Linear(2048, num_classes),
+            nn.Linear(384, num_classes),
             nn.Softmax(dim=1)
         )
 
