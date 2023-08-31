b2ba7f88ae6609ed0c5cf5a15dcd89b79cf6e656
azabelo <azabelo21old@gmail.com>
2023-08-30 22:35:09 -0400

    alternating

--- nolight.py
+++ nolight.py
@@ -35,6 +35,21 @@ class DINO(torch.nn.Module):
         z = self.teacher_head(y)
         return z
 
+class Classifier(nn.Module):
+    def __init__(self, model, num_classes):
+        super().__init__()
+        self.feature_extractor = model
+        self.classifier = nn.Sequential(
+            nn.Linear(384, num_classes),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        batch_size = x.shape[0]
+        x = x.view(batch_size, -1)
+        x = self.classifier(x)
+        return x
 
 resnet = torchvision.models.resnet18()
 backbone = nn.Sequential(*list(resnet.children())[:-1])
@@ -49,6 +64,8 @@ model = DINO(backbone, input_dim)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
+classifier = Classifier(model, 10)
+classifier.to(device)
 
 cifar_transform = transforms.Compose([
         transforms.ToTensor(),
@@ -67,6 +84,14 @@ dataloader = torch.utils.data.DataLoader(
     num_workers=8,
 )
 
+sup_loader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=64,
+    shuffle=True,
+    drop_last=True,
+    num_workers=8
+)
+
 criterion = DINOLoss(
     output_dim=2048,
     warmup_teacher_temp_epochs=5,
@@ -74,7 +99,11 @@ criterion = DINOLoss(
 # move loss to correct device because it also contains parameters
 criterion = criterion.to(device)
 
+sup_criterion = nn.CrossEntropyLoss()
+sup_criterion = sup_criterion.to(device)
+
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+sup_optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001)
 
 epochs = 10
 
@@ -83,11 +112,18 @@ for epoch in range(epochs):
     total_loss = 0
     momentum_val = cosine_schedule(epoch, epochs, 0.996, 1)
 
-    index = 0
-    for batch in dataloader:
-        print(f"batch: {index} / {len(dataloader)}")
-        index += 1
+    total_sup_loss = 0
 
+    for index in len(dataloader):
+
+        print(f"batch: {index} / {len(dataloader)}")
+
+        batch = next(iter(dataloader))
+        sup_inputs, sup_labels = next(iter(sup_loader))
+        sup_inputs = sup_inputs.to(device)
+        sup_labels = sup_labels.to(device)
+
+        print("pretrain step")
         views = batch[0]
         update_momentum(model.student_backbone, model.teacher_backbone, m=momentum_val)
         update_momentum(model.student_head, model.teacher_head, m=momentum_val)
@@ -102,6 +138,18 @@ for epoch in range(epochs):
         model.student_head.cancel_last_layer_gradients(current_epoch=epoch)
         optimizer.step()
         optimizer.zero_grad()
+
+        print("supervised step")
+        sup_optimizer.zero_grad()
+        outputs = classifier(sup_inputs)  # Forward pass
+        sup_loss = criterion(outputs, sup_labels)  # Compute the loss
+        sup_loss.backward()  # Backpropagation
+        sup_optimizer.step()  # Update weights
+
+        total_loss += loss.item()
+        total_sup_loss += sup_loss.item()
+
+
 
     avg_loss = total_loss / len(dataloader)
     print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}")
